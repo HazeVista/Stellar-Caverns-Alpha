141de9312fc796ba777bacc399ffdaa96b853ede
CosmicEcl07 <joeyachristopher@gmail.com>
2025-09-09 14:59:17 -0500

    new casings, HPC rework, schist added

---
+++ kubejs/startup_scripts/Blocks/machine_casings.js
@@ -0,0 +1,23 @@
+StartupEvents.registry('block', event => {
+
+    event.create('reinforced_stone_bricks')
+        .displayName('Reinforced Stone Bricks')
+        .hardness(5)
+        .resistance(1)
+        .soundType('stone')
+        .requiresTool(true)
+        .tagBlock('mineable/pickaxe')
+        .tagBlock('minecraft:needs_stone_tool')
+        .textureAll('kubejs:borrowed_from_start/reinforced_stone_bricks');
+
+    event.create('high_steam_machine_casing')
+        .displayName('Steel-Encased Bricks')
+        .hardness(5)
+        .resistance(1)
+        .soundType('stone')
+        .requiresTool(true)
+        .tagBlock('mineable/pickaxe')
+        .tagBlock('minecraft:needs_stone_tool')
+        .textureAll('kubejs:borrowed_from_start/high_steam_machine_casing');
+
+})
