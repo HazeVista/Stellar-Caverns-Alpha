490d11f7c27d270847be616fac1ce966e80f25da
HazeVista <zetateamhazevista@gmail.com>
2025-09-20 13:02:04 +0000

    tiny tweaks from my own playtesting experience

--- kubejs/server_scripts/power/primitive_thermal_generator.js
+++ kubejs/server_scripts/power/primitive_thermal_generator.js
@@ -2,29 +2,29 @@ ServerEvents.recipes(event => {
     // Stone burning generator
     event.recipes.gtceu.thermal_generator('primitive_stone_burning')
         .itemInputs('4x gtceu:stone_dust')
-        .inputFluids(Fluid.of('minecraft:water', 250))
-        .duration(240)
+        .inputFluids(Fluid.of('minecraft:water', 500))
+        .duration(160)
         .EUt(-8);
 
     // Light stone slurry burning
     event.recipes.gtceu.thermal_generator('primitive_light_calcareous_slurry_burning')
         .inputFluids(Fluid.of('gtceu:light_calcareous_slurry', 2000))
         .itemInputs('2x gtceu:dense_low_grade_terralite_dust')
-        .outputFluids(Fluid.of('minecraft:water', 1000))
+        .outputFluids(Fluid.of('minecraft:water', 750))
         .duration(4800)
         .EUt(-8);
 
     event.recipes.gtceu.thermal_generator('primitive_light_tuffaceous_slurry_burning')
         .inputFluids(Fluid.of('gtceu:light_tuffaceous_slurry', 2000))
         .itemInputs('2x gtceu:dense_low_grade_terralite_dust')
-        .outputFluids(Fluid.of('minecraft:water', 1000))
+        .outputFluids(Fluid.of('minecraft:water', 750))
         .duration(4800)
         .EUt(-8);
 
     event.recipes.gtceu.thermal_generator('primitive_light_granitic_slurry_burning')
         .inputFluids(Fluid.of('gtceu:light_granitic_slurry', 2000))
         .itemInputs('2x gtceu:dense_low_grade_terralite_dust')
-        .outputFluids(Fluid.of('minecraft:water', 1000))
+        .outputFluids(Fluid.of('minecraft:water', 750))
         .duration(4800)
         .EUt(-8);
 });
