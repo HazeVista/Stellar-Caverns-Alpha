8766c9178c13038574ac1ce98dc3cdc688a60a13
HazeVista <zetateamhazevista@gmail.com>
2025-09-21 07:49:47 +0000

    more tiny playtesting balances from Haze

--- kubejs/server_scripts/resources/multiblocks/hardened_blast_furnace_kiln.js
+++ kubejs/server_scripts/resources/multiblocks/hardened_blast_furnace_kiln.js
@@ -10,29 +10,29 @@ ServerEvents.recipes(event => {
         .duration(80)
 
     event.recipes.gtceu.hardened_blast_furnace('sand_to_glass_charcoal')
-        .itemInputs('8x minecraft:sand', '4x minecraft:charcoal')
+        .itemInputs('8x minecraft:sand', '1x minecraft:charcoal')
         .itemOutputs('4x minecraft:glass')
-        .duration(60)
+        .duration(80)
 
     event.recipes.gtceu.hardened_blast_furnace('iron_dust_to_ingot')
-        .itemInputs('4x gtceu:iron_dust', '4x minecraft:charcoal')
+        .itemInputs('4x gtceu:iron_dust', '1x minecraft:charcoal')
         .itemOutputs('4x minecraft:iron_ingot')
-        .duration(100)
+        .duration(80)
 
     event.recipes.gtceu.hardened_blast_furnace('copper_dust_to_ingot')
-        .itemInputs('4x gtceu:copper_dust', '4x minecraft:charcoal')
+        .itemInputs('4x gtceu:copper_dust', '1x minecraft:charcoal')
         .itemOutputs('4x minecraft:copper_ingot')
-        .duration(100)
+        .duration(80)
 
     event.recipes.gtceu.hardened_blast_furnace('lead_dust_to_ingot')
-        .itemInputs('4x gtceu:lead_dust', '4x minecraft:charcoal')
+        .itemInputs('4x gtceu:lead_dust', '1x minecraft:charcoal')
         .itemOutputs('4x gtceu:lead_ingot')
-        .duration(100)
+        .duration(80)
 
     event.recipes.gtceu.hardened_blast_furnace('iron_to_wrought_iron')
-        .itemInputs('2x minecraft:iron_ingot', '4x minecraft:charcoal')
+        .itemInputs('4x minecraft:iron_ingot', '1x minecraft:charcoal')
         .itemOutputs('4x gtceu:wrought_iron_ingot')
-        .duration(60)
+        .duration(80)
 
     event.recipes.gtceu.hardened_blast_furnace('raw_rubber_ingot')
         .itemInputs('12x gtceu:raw_rubber_dust', "4x gtceu:sulfur_dust")
@@ -45,9 +45,9 @@ ServerEvents.recipes(event => {
         .duration(160)
     
     event.recipes.gtceu.hardened_blast_furnace('wrought_iron_to_steel')
-        .itemInputs('2x minecraft:charcoal', 'gtceu:wrought_iron_ingot')
+        .itemInputs('1x minecraft:charcoal', 'gtceu:wrought_iron_ingot')
         .itemOutputs('gtceu:steel_ingot')
-        .duration(400)
+        .duration(600)
         
 });
 
